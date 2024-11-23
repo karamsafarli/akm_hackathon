31e3854285fe96a606793c908cf13b4ef4af7054
karamsafarli <karamslee007@gmail.com>
2024-11-23 14:14:35 +0400

    login and register page folders

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,35 @@
-import './App.css'
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import Login from './pages/login/Login';
+import Register from './pages/register/Register';
 
 function App() {
+  return (
+    <BrowserRouter>
+      <Pages />
+    </BrowserRouter>
+  );
+}
+
+function Pages() {
+  const location = useLocation();
+  const isLoginPage = location.pathname === "/login" || '/register';
 
   return (
     <>
-      
+      {!isLoginPage ? (
+        <>
+          <Routes>
+
+          </Routes>
+        </>
+      ) : (
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      )}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
